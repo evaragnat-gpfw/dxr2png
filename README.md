a70cc0c9cdd0cb7eb6fb22812c660c1cb6e0951d
Emmanuel Varagnat <evaragnat@gopro.com>
2023-02-10 11:18:48 +0100

    Allow dumping all planes or just selected ones

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Usage
 
-`dxr2png GOPR3241.dxr Gr` will generate a `GOPR3241-Gr.png` file with Gr image plane.
+```
+dxr2png GOPR3241.dxr    # will produce GOPR3241.png with all planes
+
+dxr2png GOPR3241.dxr Gr # will produce GOPR3241-Gr.png with only Gr plane
+
+dxr2png --no-binning GOPR3241.dxr Gr # will produce GOPR3241-Gr.png with only Gr plane
+                                     # and all other planes black
+```
 
 # Tests
 
-Extraction can be easily tested with DXR from sensor pattern.
-
+Extraction and correctness can be easily tested with DXR from sensor pattern.
--- dxr.c
+++ dxr.c
@@ -15,6 +15,13 @@ int save_png(const char* filename,
              unsigned char* data, int pitch,
              int transform);
 
+
+#define Bayer_R   0x1
+#define Bayer_Gr  0x2
+#define Bayer_Gb  0x4
+#define Bayer_B   0x8
+#define Bayer_ALL (Bayer_R | Bayer_Gr | Bayer_B | Bayer_Gb)
+
 // From :
 //   https://goproinc.atlassian.net/wiki/spaces/ISG/pages/18819332/DXR+file+format+specification
 struct DxrHeader {
@@ -83,20 +90,13 @@ const char* planarity[] = {
     "semi-planar"
 };
 
-enum Bayer {
-    Bayer_R,
-    Bayer_Gr,
-    Bayer_Gb,
-    Bayer_B
-};
-
 int main(int argc, char*argv[]) {
     int n;
     struct DxrHeader hdr;
     unsigned int bits;
     uint8_t* png_data;
     uint8_t* px;
-    enum Bayer plane;
+    uint8_t  planes;
     FILE* fd;
     char* dxrpath;
     char* pngpath;
@@ -112,16 +112,24 @@ int main(int argc, char*argv[]) {
        binning = false;
        opt++;
     }
-
-    if (argc + opt < 3) {
-        fprintf(stderr, "ERROR: no plane [R,Gr,Gb,B] specified\n");
+    else if (strcmp(argv[1], "--help") == 0) {
+        printf("usage: dxr2png [--no-binning] filename {R,Gr,Gb,B}\n");
+        return 0;
+    }
+    else if (strncmp(argv[1], "--", 2) == 0) {
+        fprintf(stderr, "ERROR: unknown option %s\n", argv[1]);
         return 1;
     }
 
-    if (strcasecmp(argv[2 + opt], "R") == 0)       plane = Bayer_R;
-    else if (strcasecmp(argv[2 + opt], "Gr") == 0) plane = Bayer_Gr;
-    else if (strcasecmp(argv[2 + opt], "Gb") == 0) plane = Bayer_Gb;
-    else if (strcasecmp(argv[2 + opt], "B") == 0)  plane = Bayer_B;
+    if (argc + opt < 3) {
+        fprintf(stderr, "INFO: no plane [R,Gr,Gb,B] specified, extracting all planes\n");
+        planes = Bayer_ALL;
+        binning = false;
+    }
+    else if (strcasecmp(argv[2 + opt], "R") == 0)  planes = Bayer_R;
+    else if (strcasecmp(argv[2 + opt], "Gr") == 0) planes = Bayer_Gr;
+    else if (strcasecmp(argv[2 + opt], "Gb") == 0) planes = Bayer_Gb;
+    else if (strcasecmp(argv[2 + opt], "B") == 0)  planes = Bayer_B;
     else {
         fprintf(stderr, "ERROR: unknown plane\n");
         return 1;
@@ -190,10 +198,13 @@ int main(int argc, char*argv[]) {
         n = fread(buf, 3, 1, fd);
         assert(n == 1);
 
-        // order is determined by type (here Bayer0 is Gb, B, R, Gr
+        // order is determined by type (here Bayer0 is Gb, B, R, Gr)
         b1 = (uint16_t)buf[0] | ((uint16_t)buf[1] & 0x000fU) << 8;
         b2  = ((uint16_t)buf[1] >> 4) | ((uint16_t)buf[2] << 4);
 
+        // convert to 8 -bit
+        b1 = b1 >> 4;
+        b2 = b2 >> 4;
 #if 0
         printf("%5d: %d: %02x %02x %02x : %03x %03x\n",
                off,
@@ -202,17 +213,13 @@ int main(int argc, char*argv[]) {
 #endif
         if (off % (2 * hdr.width) < hdr.width) {
             // Gb and B
-            if (plane == Bayer_Gb) { *px++ = (b1 >> 4) & 0xFF; if (!binning) *px++ = 0; }
-            if (plane == Bayer_B)  { if (!binning) *px++ = 0; *px++ = (b2 >> 4) & 0xFF; }
-            if (plane == Bayer_R || plane == Bayer_Gr)
-                if (!binning) { *px++ = 0; *px++ = 0; }
+            if (planes & Bayer_Gb || !binning) { *px++ = (planes & Bayer_Gb) ? b1 : 0; }
+            if (planes & Bayer_B  || !binning) { *px++ = (planes & Bayer_B)  ? b2 : 0; }
         }
         else {
             // R and Gr
-            if (plane == Bayer_R)  { *px++ = (b1 >> 4) & 0xFF; if (!binning) *px++ = 0; }
-            if (plane == Bayer_Gr) { if (!binning) *px++ = 0; *px++ = (b2 >> 4) & 0xFF; }
-            if (plane == Bayer_Gb || plane == Bayer_B)
-                if (!binning) { *px++ = 0; *px++ = 0; }
+            if (planes & Bayer_R  || !binning) { *px++ = (planes & Bayer_R) ? b1 : 0; }
+            if (planes & Bayer_Gr || !binning) { *px++ = (planes & Bayer_R) ? b2 : 0; }
         }
     }
 
@@ -232,7 +239,10 @@ int main(int argc, char*argv[]) {
         fprintf(stderr, "ERROR: RAW file is not a .DXR file\n");
         return 1;
     }
-    sprintf(pngpath + len - 4, "-%s.png", argv[2 + opt]);
+    if ((planes & Bayer_ALL) == Bayer_ALL)
+        sprintf(pngpath + len - 4, ".png");
+    else
+        sprintf(pngpath + len - 4, "-%s.png", argv[2 + opt]);
 
     printf("Saving to PNG...\n");
 
